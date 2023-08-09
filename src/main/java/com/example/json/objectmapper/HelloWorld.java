a878e10750cd6e801e82e23def1efee72ad1fb55
LEO_TSAI\leots <leotsai@glints.community>
2023-08-09 10:33:54 +0800

    update

--- src/main/java/com/example/json/objectmapper/HelloWorld.java
+++ src/main/java/com/example/json/objectmapper/HelloWorld.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
-    @GetMapping("/test")
+    @GetMapping()
     public String test(){
         return  "HelloWorld";
     }
